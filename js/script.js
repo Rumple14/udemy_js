07f57f0e47e337f03a2a1461b25fe58091558348
Yunus <yunuskussiev2007@mail.ru>
2022-01-17 14:18:03 +0300

    lesson 13

--- js/script.js
+++ js/script.js
@@ -1,19 +1,34 @@
 'use strict';
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+// if (4 == 4) {
+//     console.log('Ok!');
+// } else {
+//     console.log('Error!')
+// }
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    private: false
-};
+// const num = 50;
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько оцените его?', '');
+// if (num < 49) {
+//     console.log('Error');
+// } else if(num > 100) {
+//     console.log('Много');
+// } else {
+//     console.log('Ok')
+// }
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+// (num == 50) ? console.log('Ok.') : console.log('Error');
+
+// switch (num) {
+//     case 49: 
+//         console.log('Неверно');
+//         break;
+//     case 100: 
+//         console.log('Неверно');
+//         break;
+//     case 50:
+//         console.log('В точку!');
+//         break;
+//     default:
+//         console.log('Не в этот раз');
+//         break;
+// }
