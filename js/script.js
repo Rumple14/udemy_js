f646d637396764995f45f9f91965ec729f7ef1d1
Yunus <yunuskussiev2007@mail.ru>
2022-01-17 13:52:51 +0300

    task 2

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
 'use strict';
 
 const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?');
+
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    private: false
+};
