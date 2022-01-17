61692ada02e310980b136f5458649f84c1e0df37
Yunus <yunuskussiev2007@mail.ru>
2022-01-17 22:59:14 +0300

    lesson 15, task 4

--- js/script.js
+++ js/script.js
@@ -25,12 +25,31 @@ const personalMovieDB = {
 
 // console.log(personalMovieDB);
 
-if (personalMovieDB.count < 10) {
-    console.log('Просмотрено довольно мало фильмов');
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-    console.log('Вы классический зритель');
-} else if(personalMovieDB.count >=30) {
-    console.log('Вы киноман')
-} else {
-    console.log('Произошла ошибка')
-}
+
+
+// if (personalMovieDB.count < 10) {
+//     console.log('Просмотрено довольно мало фильмов');
+// } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+//     console.log('Вы классический зритель');
+// } else if(personalMovieDB.count >=30) {
+//     console.log('Вы киноман');
+// } else {
+//     console.log('Произошла ошибка');
+// };
+
+
+
+// while (personalMovieDB.count < 10) {
+//     console.log('Просмотрено довольно мало фильмов');
+//     break;
+// };
+
+// while (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+//     console.log('Вы классический зритель');
+//     break;
+// };
+
+// while (personalMovieDB.count >=30) {
+//     console.log('Вы киноман');
+//     break;
+// };
