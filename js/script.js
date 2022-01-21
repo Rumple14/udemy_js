4bc4a09222dec9757076109ad53da3410d57bde2
Yunus <yunuskussiev2007@mail.ru>
2022-01-21 22:28:39 +0300

    lesson 16

--- js/script.js
+++ js/script.js
@@ -1,55 +1,33 @@
 "use strict";
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    private: false
-};
-
-// for (let i = 0; i < 2; i++) {
-//     let a = prompt('Один из последних просмотренных фильмов?', ''),
-//         b = prompt('На сколько оцените его?', '');
-
-//     if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-//         personalMovieDB.movies[a] = b;
-//         console.log('done');
-//     } else {
-//         console.log('error');
-//         i--;
-//     }
+// function showFirstMessage(text) {
+//     console.log(text);
+//     num = 10;
+//     console.log(num);
 // }
 
-// console.log(personalMovieDB);
+// showFirstMessage('Hello World!');
+// console.log(num);
 
+// function calc(a, b) {
+//     return (a + b);
+// }
 
+// console.log(calc(4, 3));
+// console.log(calc(5, 6));
+// console.log(calc(10, 6));
 
-// if (personalMovieDB.count < 10) {
-//     console.log('Просмотрено довольно мало фильмов');
-// } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-//     console.log('Вы классический зритель');
-// } else if(personalMovieDB.count >=30) {
-//     console.log('Вы киноман');
-// } else {
-//     console.log('Произошла ошибка');
+// function ret() {
+//     let num = 50;
+//     return num;   
+// }
+
+// const anotherNum = ret();
+// console.log(anotherNum);
+
+// const logger = function() {
+//     console.log('Hello');
 // };
+// logger();
 
-
-
-// while (personalMovieDB.count < 10) {
-//     console.log('Просмотрено довольно мало фильмов');
-//     break;
-// };
-
-// while (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-//     console.log('Вы классический зритель');
-//     break;
-// };
-
-// while (personalMovieDB.count >=30) {
-//     console.log('Вы киноман');
-//     break;
-// };
+// const calc = (a, b) => a + b;
